be2c46759c0c115ac1df992dda5ce942b84f1a98
Phạm Tuấn Kiệt <ptkit1.0@gmail.com>
2026-02-25 11:11:43 +0700

    update src main

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,9 @@ async function bootstrap() {
     }),
   );
   app.enableCors();
-  const port = parseInt(process.env.PORT || '8080', 10);
+  const port = Number(process.env.PORT) || 8080;
   await app.listen(port, '0.0.0.0');
-  console.log(`Application is running on: http://0.0.0.0:${port}`);
+  console.log(`Listening on ${port}`);
 }
 
 bootstrap().catch((err) => {
